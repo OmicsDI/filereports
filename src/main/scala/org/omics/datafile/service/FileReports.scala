9f8e3fd749a42e350493624b17d5a87fb517b015
Gaurhari <gaurharidass@gmail.com>
2019-04-09 16:24:44 +0100

    updated to make it strcutured code

--- src/main/scala/org/omics/datafile/service/FileReports.scala
+++ src/main/scala/org/omics/datafile/service/FileReports.scala
@@ -6,50 +6,30 @@ import org.apache.spark.sql.{Column, Encoders, SparkSession}
 import com.mongodb.spark.sql._
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.functions.explode
+import org.omics.datafile.utils.Utilities
 
 object FileReports {
 
 
   def main(args:Array[String]): Unit ={
-    val sparkSession = SparkSession.builder()
-      .appName("mongozips")
-      .master("local[*]")
-      .getOrCreate()
 
+    val sparkSession = SparkService.getSparkSession()
 
-    val zipDf = sparkSession.read.mongo(readConfig)
-
+    val zipDf = sparkSession.read.mongo(MongoSparkService.getMongoReadConfig)
 
     val explodedMapDf =  zipDf.select(explode(zipDf.col("files")) as (Seq("acc", "fileList")))
 
     val explodedFiles =   explodedMapDf.select(explode(explodedMapDf.col("fileList")) as "files")
 
     import org.apache.spark.sql.functions.udf
-    val getExtensionUdf = udf[String, String](getExtension)
+    val getExtensionUdf = udf[String, String](Utilities.getExtension)
 
-    val getWholeExtensionUdf = udf[String, String](getSecondPart)
+    val getWholeExtensionUdf = udf[String, String](Utilities.getSecondPart)
 
     explodedFiles
       .withColumn("fileExtension",getWholeExtensionUdf(explodedFiles("files")))
       .groupBy("fileExtension").count().coalesce(1).write.csv("/home/gaur/extensions.csv")
 
-  }
-
-  val reg_ex = """.*\.(\w+)""".r
-
-  def getExtension(dt:String):String={
-    dt match {
-      case reg_ex(ext) => ext
-      case _ =>  "NoExtension"
-    }
-  }
-
-  def getSecondPart(in:String):String = {
-    val fileName = in.split("/").last
-    if(fileName.contains("."))
-      fileName.substring(fileName.indexOf('.'), fileName.length)
-    else
-      "NoExtension"
   }
 
 }
