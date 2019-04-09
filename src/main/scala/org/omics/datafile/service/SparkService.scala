87bee395c733c723e721084ab5fbbad6db26c68a
Gaurhari <gaurharidass@gmail.com>
2019-04-09 16:25:59 +0100

    added spark service for spark operations

--- src/main/scala/org/omics/datafile/service/SparkService.scala
+++ src/main/scala/org/omics/datafile/service/SparkService.scala
@@ -1,5 +1,15 @@
 package org.omics.datafile.service
 
+import org.apache.spark.sql.SparkSession
+
 object SparkService {
+
+  def getSparkSession(): SparkSession ={
+    SparkSession.builder()
+      .appName("mongozips")
+      .master("local[*]")
+      .getOrCreate()
+
+  }
 
 }
