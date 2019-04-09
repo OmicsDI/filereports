d25d2055ac951d5051427a5ddf804c5754781ef4
Gaurhari <gaurharidass@gmail.com>
2019-04-09 16:25:18 +0100

    added utilities

--- src/main/scala/org/omics/datafile/utils/Utilities.scala
+++ src/main/scala/org/omics/datafile/utils/Utilities.scala
@@ -1,5 +1,24 @@
 package org.omics.datafile.utils
 
+import org.omics.datafile.service.FileReports
+
 object Utilities {
+
+  val reg_ex = """.*\.(\w+)""".r
+
+  def getExtension(dt:String):String={
+    dt match {
+      case reg_ex(ext) => ext
+      case _ =>  "NoExtension"
+    }
+  }
+
+  def getSecondPart(in:String):String = {
+    val fileName = in.split("/").last
+    if(fileName.contains("."))
+      fileName.substring(fileName.indexOf('.'), fileName.length)
+    else
+      "NoExtension"
+  }
 
 }
